452117368ea4a3a337c7fb151db57ff3ec4f35b6
Caden Wilcox <wilcox4357@gmail.com>
2022-04-21 01:29:58 -0700

    get player id's and add total pts per season pts is higher than should be though

--- API/nba_API_test.py
+++ API/nba_API_test.py
@@ -1,26 +1,41 @@
 import requests
 
-url = "https://api-nba-v1.p.rapidapi.com/games"
-
-querystring = {"date":"2022-04-17"}
+# url = "https://api-nba-v1.p.rapidapi.com/games"
+url = "https://api-nba-v1.p.rapidapi.com/players/statistics"
+# querystring = {"date":"2022-04-17"}
 # querystring = {"season":"2021"}
+player_stats = {"season":2019, "id":319}
+player_name = {"name": "Lillard"}
 
 headers = {
 	"X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
 	"X-RapidAPI-Key": "6fe48baa96mshb163a9419d8ad01p19250bjsn43258ad3dc6c"
 }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+response = requests.request("GET", url, headers=headers, params=player_stats)
+print(response)
 
 res = response.json()
-# print(res)
-print(f"{res['response'][1]['teams']['visitors']['name']} @ {res['response'][1]['teams']['home']['name']}")
-print(f"Score: {res['response'][1]['teams']['visitors']['name']}: {res['response'][1]['scores']['visitors']['points']} {res['response'][1]['teams']['home']['name']}: {res['response'][1]['scores']['home']['points']}")
-
-def display_who_won(res):
-	if  res['response'][0]['scores']['visitors']['points'] > res['response'][0]['scores']['home']['points']:
-		print(f"{res['response'][0]['teams']['visitors']['name']} Beat {res['response'][0]['teams']['home']['name']}\n{res['response'][0]['scores']['visitors']['points']} To {res['response'][0]['scores']['home']['points']}")
+res = res['response']
+print(res)
+# print(res[0]['player']['firstname'], res[0]['player']['lastname'])
+# print(res[66]['points'])
+pts = 0
+for dict in res:
+	if dict['points'] == None:
+		continue
 	else:
-		print(f"{res['response'][0]['teams']['home']['name']} Beat {res['response'][0]['teams']['visitors']['name']}\n{res['response'][0]['scores']['home']['points']} To {res['response'][0]['scores']['visitors']['points']}")
+		print(dict['points'])
+		pts += dict['points']
+print(pts)
 
-display_who_won(res)
+# print(f"{res['response'][1]['teams']['visitors']['name']} @ {res['response'][1]['teams']['home']['name']}")
+# print(f"Score: {res['response'][1]['teams']['visitors']['name']}: {res['response'][1]['scores']['visitors']['points']} {res['response'][1]['teams']['home']['name']}: {res['response'][1]['scores']['home']['points']}")
+
+# def display_who_won(res):
+# 	if  res['response'][0]['scores']['visitors']['points'] > res['response'][0]['scores']['home']['points']:
+# 		print(f"{res['response'][0]['teams']['visitors']['name']} Beat {res['response'][0]['teams']['home']['name']}\n{res['response'][0]['scores']['visitors']['points']} To {res['response'][0]['scores']['home']['points']}")
+# 	else:
+# 		print(f"{res['response'][0]['teams']['home']['name']} Beat {res['response'][0]['teams']['visitors']['name']}\n{res['response'][0]['scores']['home']['points']} To {res['response'][0]['scores']['visitors']['points']}")
+
+# display_who_won(res)
