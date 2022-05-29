6bafe452590f2a15523b42f961e578e882aaed3d
Caden Wilcox <wilcox4357@gmail.com>
2022-05-28 22:32:17 -0700

    started with adding a user from the front end

---
+++ flask_fullstack/apis/coin_api/flask_app/controllers/user_controller.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template,redirect,jsonify,request,session
+from flask_app import app, db
+import requests
+from flask_app.models.tables import User, Purchase
+
+@app.route('/register/login')
+def register_login():
+    return render_template('sign_in_up.html')
+
+@app.route('/register/user', methods=['POST'])
+def register():
+    new_user = User(first_name = request.form['fist_name'], last_name = request.form['last_name'], email = request.form['email'], password = request.form['password'])
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/home')
--- flask_fullstack/apis/coin_api/server.py
+++ flask_fullstack/apis/coin_api/server.py
@@ -1,4 +1,4 @@
 from flask_app import app
-from flask_app.controllers import dashboard_controller
+from flask_app.controllers import dashboard_controller, user_controller
 if __name__ == "__main__":
     app.run(debug=True)
